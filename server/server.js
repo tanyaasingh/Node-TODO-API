f24686796bb03103dad58bf38bf3fb7865175006
Tanyaa Singh <tanyaasinghts@gmail.com>
2019-01-13 02:00:22 +0530

    add get todo route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,13 @@ app.post('/todos',(req,res)=>{
         res.status(400).send(e)
     })
 })
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos})
+    },(e)=>{
+        res.status(400).send(e)
+    })
+})
 
 
 
